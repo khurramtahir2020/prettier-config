5c2108a764c0c5037c2d0864eb73597b3e861cd1
Khurram Tahir <khurram.tahir@knab.nl>
2020-03-24 08:05:55 +0100

    prettier config

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  printWidth: 80,
+};
